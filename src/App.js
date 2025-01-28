85b25c95b0d2904c639caf93bdba48308839c2e7
Pallavi <90495921+pallavi310501@users.noreply.github.com>
2025-01-28 16:15:16 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My Portfolio</h1>
-        <h2>Hi, I'm Meghana Katraju! <span role="img" aria-label="wave">👋</span></h2>
+        <h2>Hi, I'm Pallavi Yellisetty! <span role="img" aria-label="wave">👋</span></h2>
         <div className="profile-picture">
-          <img src="/images/my-picture.jpg" alt="Meghana Katraju" />
+          <img src="https://imgur.com/a/0MKq4bE" alt="Pallavi Yellisetty" />
         </div>
         <div className="intro">
           <h3>About Me</h3>
